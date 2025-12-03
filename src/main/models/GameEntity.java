e72607c13476f5a14acf911776206dbfdcf7d566
Alquarim <alquirm1@tcnj.edu>
2025-12-03 14:18:07 -0500

    Update GameEntity.java

--- src/main/models/GameEntity.java
+++ src/main/models/GameEntity.java
@@ -3,4 +3,24 @@ package main.models;
 public class GameEntity {
     private String name;
     private String description;
+
+    public void setName(String entityName){
+        name = entityName;
+    }
+    
+    public void setDescription(String entityDescription){
+        description = entityDescription;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String toString() {
+        return "{" + getName() + ", " + getDescription() + "}";
+    }
 }
