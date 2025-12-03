29c142c92cf2bea48349c16978502ac518613fb4
ShawnSmith7 <smiths40@tcnj.edu>
2025-12-02 20:59:15 -0500

    added room constructor, getters, and setters

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,7 +1,14 @@
 package main;
 
+import java.util.HashMap;
+
+import main.models.Room;
+
 public class Game {
+    private static HashMap<Integer, Room> rooms;
+    private static WorldLoader worldLoader;
+    private static SaveManager saveManager;
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to the Text Adventure!");
     }
 }
--- src/main/models/Room.java
+++ src/main/models/Room.java
@@ -1,5 +1,83 @@
 package main.models;
 
+import java.util.HashMap;
+import java.util.ArrayList;
+
 public class Room {
-    
+    // instance data
+
+    private int roomID;
+    private String name;
+    private String description;
+    private HashMap<String, Integer> exits;
+    private ArrayList<Item> itemsInRoom;
+    private ArrayList<Monster> monstersInRoom;
+
+    // constructors
+
+    public Room(int roomID,
+        String name,
+        String description,
+        HashMap<String, Integer> exits,
+        ArrayList<Item> itemsInRoom,
+        ArrayList<Monster> monstersInRoom) {
+        setRoomID(roomID);
+        setName(name);
+        setDescription(description);
+        setExits(exits);
+        setItemsInRoom(itemsInRoom);
+        setMonstersInRoom(monstersInRoom);
+    }
+
+    // getters
+
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public HashMap<String, Integer> getExits() {
+        return exits;
+    }
+
+    public ArrayList<Item> getItemsInRoom() {
+        return itemsInRoom;
+    }
+
+    public ArrayList<Monster> getMonstersInRoom() {
+        return monstersInRoom;
+    }
+
+    // setters
+
+    public void setRoomID(int roomID) {
+        this.roomID = roomID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setExits(HashMap<String, Integer> exits) {
+        this.exits = exits;
+    }
+
+    public void setItemsInRoom(ArrayList<Item> itemsInRoom) {
+        this.itemsInRoom = itemsInRoom;
+    }
+
+    public void setMonstersInRoom(ArrayList<Monster> monstersInRoom) {
+        this.monstersInRoom = monstersInRoom;
+    }
 }
