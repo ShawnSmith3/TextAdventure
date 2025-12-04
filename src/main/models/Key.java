1fc75284e4783f12a2f7703d3358e8a708e76fc8
Alquarim <alquirm1@tcnj.edu>
2025-12-03 21:39:09 -0500

    Update Key.java

--- src/main/models/Key.java
+++ src/main/models/Key.java
@@ -1,9 +1,51 @@
 package main.models;
 
 public class Key {
-   private int keyID;
+    private String name;
+    private String description;
+    private double weight;
+    private int keyID;
 
-   void interact(Player player){
-    
+   public Key(String name, String description, double weight, int keyID){
+    this.name = name;
+    this.description = description;
+    this.weight = weight;
+    this.keyID = keyID;
+   }
+
+  public void setName(String keyName){
+    keyName = name;
+  }
+
+  public void setDescription(String keyDescription){
+    keyDescription = description;
+  }
+
+  public void setWeight(double keyWeight){
+    keyWeight = weight;
+  }
+
+  public void setKeyID(int keyID){
+    this.keyID = keyID; 
+  }
+
+  public String getName(){
+    return name;
+  }
+  
+  public String getDescription(){
+    return description;
+  }
+
+  public double getWeight(){
+    return weight;
+  }
+
+  public int getKeyID(){
+    return keyID;
+  }
+
+  void interact(Player player){
+
    }
 }
