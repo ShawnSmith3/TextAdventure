68c445bcb69f03cf249de17b28d5d4f98a46dae7
Alquarim <alquirm1@tcnj.edu>
2025-12-04 13:06:22 -0500

    Update Monster.java

--- src/main/models/Monster.java
+++ src/main/models/Monster.java
@@ -1,5 +1,61 @@
 package main.models;
 
-public class Monster {
+public class Monster{
+    private String name;
+    private String description;
+    private int health;
+    private int damage;
+    private Item loot;
+
+    public Monster(String name, String description, int health, int damage, Item loot){
+        this.name = name;
+        this.description = description;
+        this.health = health;
+        this.damage = damage;
+        this.loot = loot;
+    }
+
+    public void setName(String monsterName){
+        name = monsterName;
+    }
     
+    public void setDescription(String monsterDescription){
+        description = monsterDescription;
+    }
+
+    public void setHealth(int monsterHealth){
+        health = monsterHealth;
+    }
+
+    public void setDamage(int damage){
+        this.damage = damage;
+    }
+
+    public void setLoot(Item loot){
+        this.loot = loot;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getHealth(){
+        return health;
+    }
+
+    public int getDamage(){
+        return damage;
+    }
+
+    public Item getLoot(){
+        return loot;
+    }
+
+    public String toString() {
+        return "{" + getName() + ", " + getDescription() + ", " + getHealth() + ", " + getDamage() + ", " + getLoot() + "}";
+    }
 }
