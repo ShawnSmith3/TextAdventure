bf209247daf1ea2b08db18abe0eeb2f6b8e2646d
ShawnSmith7 <smiths40@tcnj.edu>
2025-12-05 12:54:40 -0500

    added room description

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,36 +1,57 @@
 package main;
 
-import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.*;
 
-import main.models.Room;
-import main.models.Player;
-import main.models.Item;
+import main.models.*;
 
 public class Game {
     private static HashMap<Integer, Room> rooms;
     private static Player player;
+    private static Scanner scan;
 
     public static void main(String[] args) {
         // load game
         rooms = WorldLoader.loadRooms("src/world/rooms.csv");
         player = WorldLoader.loadObjects("src/world/objects.csv", rooms);
+        scan = new Scanner(System.in);
         
         System.out.println("Welcome to the Text Adventure!");
 
         // game loop
         while (true) {
+            // describe room
             Room room = rooms.get(player.getLocation());
-            System.out.println("--- " + room.getName() + " ---\n"
-                            + room.getDescription() + "\n");
+            System.out.print("--- " + room.getName() + " ---\n"
+                            + room.getDescription() + "\n\n"
+                            + "You see: ");
             
+            // describe objects in room
             ArrayList<Item> itemsInRoom = room.getItemsInRoom();
-            //if (itemsInRoom.size() > 0)
-                //for (Item item : itemsInRoom)
-                
+            ArrayList<Monster> monstersInRoom = room.getMonstersInRoom();
+            if (itemsInRoom.size() > 0 || monstersInRoom.size() > 0) {
+                System.out.println();
+                for (Item item : itemsInRoom)
+                    System.out.println("- " + item.getName());
+                for (Monster monster : monstersInRoom)
+                    System.out.println("- " + monster.getName());
+            } else
+                System.out.println("Nothing");
+            System.out.println();
+
+            // describe exits from room
+            System.out.print("Exits are: ");
+            HashMap<String, Integer> exits = room.getExits();
+            for (Map.Entry<String, Integer> exit : exits.entrySet())
+                if (exit.getValue() != -1)
+                    System.out.print(exit.getKey() + " ");
+            System.out.println("\n");
+
+            // command loop
+            
                 
             break;
         }
         SaveManager.saveGame(rooms, player);
+        scan.close();
     }
 }
