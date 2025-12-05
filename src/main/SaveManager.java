4cf70acab94fc22ffbd815980d318f515beae3f9
Alquarim <alquirm1@tcnj.edu>
2025-12-04 23:18:44 -0500

    Matthew

--- src/main/SaveManager.java
+++ src/main/SaveManager.java
@@ -1,5 +1,22 @@
 package main;
 
+import java.io.*;
+import java.util.*;
+import main.models.Player;
+import main.models.Room;
+
+
+
 public class SaveManager {
     
+    public static void saveGame(HashMap<Integer, Room> rooms, Player player){
+        try(PrintWriter writer = new PrintWriter(new FileWriter("src/world/savegame.csv"))){
+            writer.println(player.getData());
+            writer.close();
+        }
+        catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+        
+    }
 }
